653c52651b9d09ec659910c2fc36779dd227f53d
costamay <titusouko@gmail.com>
2024-08-06 11:24:42 +0300

    initial commit

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,10 +1,38 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 function UserDetails() {
     const [user, setUser] =useState({})
 
+    const { id } = useParams()
+    
+
+    useEffect(()=>{
+        fetch(`http://localhost:3000/users/${id}`)
+        .then(res => res.json())
+        .then(user => setUser(user))
+        .catch(error => console.log(error))
+    },[id])
+
+    // console.log(user)
+
   return (
-    <div>UserDetails</div>
+    <div>
+        <h1>UserDetails</h1>
+        <h1>Name: {user.name}</h1>
+        <h3>Username: {user.username}</h3>
+        <h5>Phone: {user.phone}</h5>
+        <h3>Company: {user.company.name}</h3>
+        <div>
+            <div>
+                <h3>Address:</h3>
+                <h5>Street: {user.address.street}</h5>
+                <hr />
+                <h5>City: {user.address.city}</h5>
+            </div>
+        </div>
+
+    </div>
   )
 }
 
